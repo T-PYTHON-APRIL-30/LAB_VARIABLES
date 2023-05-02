273749739c5b7f29762b239cbd82f0f26afe03eb
lena <lenaashayban@gmail.com>
2023-05-02 12:15:56 +0300

    labNum3

---
+++ labVariables.py
@@ -0,0 +1,15 @@
+
+# Define a variable that has a value of your favourite football team.
+favourite_football = "don't have one"
+
+# Define a variable “x“ has an integer value and a variable “y” has a float value.
+x:int  = 100
+y:float = 3.3
+
+# Assign One Value to Multiple Variables.
+var1 = var2 = 364
+
+# Assign multiple Values to Multiple Variables.
+a, b, c, d = 1, 3.5, "lena", 'shayban'
+
+print(x,y,favourite_football,a,b,c,d,var1)
