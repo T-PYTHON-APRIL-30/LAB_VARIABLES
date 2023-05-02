4f2f6d7408fba354ba3355c9010b03b419ec489d
amalsaab <441102683@student.ksu.edu.sa>
2023-05-02 14:03:08 +0300

    create arithmatic_operation.py and string operation.py

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-myVT = "Manchester United"
+myVT : str = "Manchester United"
+'''
+we havr 3 way to deal with on var nam:
+'''
+#snake_case ()
+favorite_team = "AL Ittihad"
+#camelCase
+favoriteTeam =""
+#PascalCase ()
+#SomeCase
 
 x:int = 9
 y:float = 9.2
---
+++ string_operation.py
@@ -0,0 +1,11 @@
+
+
+student_name = "Abdullah"
+course = "Building websites with Python"
+random_number = "30"
+
+print(student_name.upper)
+print()
+print()
+print()
+print()
