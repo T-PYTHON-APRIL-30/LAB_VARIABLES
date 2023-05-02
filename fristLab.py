9c64566ea236ea65ea76e415e6b56f836f93e725
khalid <10abha10@gmail.com>
2023-05-02 12:05:26 +0300

    solved

---
+++ fristLab.py
@@ -0,0 +1,8 @@
+footBall_team="ittihad"
+x:int=10
+y:float=12.5
+
+name1=name2=name3="khalid"
+fristName,midName,lastName="khalid","ibrahim","Alhimali"
+print(footBall_team+"\n"+name2+"\n"+f"{x}\n{y}"
+      "\n"+fristName+"\n"+midName+"\n"+lastName)
