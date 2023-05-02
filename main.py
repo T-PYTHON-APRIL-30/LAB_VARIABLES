6fc41269595b3cecb28d8f566bc10e00b516a9ea
Bakr Hawsawi <122125253+Bakr40@users.noreply.github.com>
2023-05-02 11:59:23 +0300

    Finshed the lab

---
+++ main.py
@@ -0,0 +1,23 @@
+favourite_football = "real madrid"
+x = 4
+y = 10.0
+v1 = v2 = v3 = 2000
+s1 = s2 = s3 = "Salem"
+
+Name , Last_Name , birthdate , Fav_numb = "Bakr" , "Hawsawi" , "10/3/2000" , 4
+
+print(favourite_football)
+print(x)
+print(y)
+print(v1)
+print(v3)
+print(s3)
+print(s2)
+print(f'my name is {Name} {Last_Name}') 
+print(birthdate)
+print(Fav_numb)
+
+
+
+
+
