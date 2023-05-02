9f219dc7b232cbd15399ea83d7972de05544f3b8
1947860@uj.edu.sa <1947860@uj.edu.sa>
2023-05-02 12:06:52 +0300

    First -lab

---
+++ solve.py
@@ -0,0 +1,11 @@
+#my favourite football team.
+liverpool= int(5)
+#Define a variable “x“ has an integer value
+x= int(6)
+#variable “y” has a float value.
+y=float(3.23)
+#Assign One Value to Multiple Variables (choose random names & values).
+faisal = fahad= alhussain= 5
+#Assign multiple Values to Multiple Variables (choose random names & values).
+number_1 , number_2, number_3 = 2,3,4
+print(fahad)
