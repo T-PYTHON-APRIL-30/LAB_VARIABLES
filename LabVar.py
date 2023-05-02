f809497726efcfa0bd64a5026c3fa3d19cb24a29
NouraAldayhani <nouraa.sultan9@gmail.com>
2023-05-02 12:01:46 +0300

    solved the lab

---
+++ LabVar.py
@@ -0,0 +1,9 @@
+# variable hat has a value of my favourite football team.
+my_fav_team = "AL-HILAL"
+# variable “x“ has an integer value and a variable “y” has a float value
+x = 5
+y = 4.3
+# One Value to Multiple Variables 
+var1 = var2 ="Hello"
+# Multiple Values to Multiple Variables 
+name , age = "Noura" , 24
