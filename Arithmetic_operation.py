2f0504073fae8284e7de014d5e5a122cd3bd407a
amalsaab <441102683@student.ksu.edu.sa>
2023-05-02 14:05:35 +0300

    create arithmatic_operation.py and string operation.py

--- Arithmetic_operation.py
+++ Arithmetic_operation.py
@@ -1,34 +1,34 @@
-#arithmatic operation in python 
+# arithmatic operation in python
 
-#addtion
+# addtion
 print("5+6= ", 5+6)
 
 addition_result = 5+6
 
-#sub
+# sub
 print("5-6= ", 5-6)
 
 sub_result = 5-6
 
-#mult (if we went float number we should put 5.0*6)
+# mult (if we went float number we should put 5.0*6)
 print("5x6= ", 5*6)
 
 mult_result = 5*6
 
-#div (result of div is float)
+# div (result of div is float)
 print("6/2= ", 6/2)
 
 div_result = 5/2
 
-#float div //
-print("5//2= ", 5//2) #output epual 2
+# float div //
+print("5//2= ", 5//2)  # output epual 2
 
 addition_result = 5//6
 
-# remainder operation modulo % 
-print("7%6= ", 7%6)
+# remainder operation modulo %
+print("7%6= ", 7 % 6)
 
-addition_result = 7%6
+addition_result = 7 % 6
 
 # raising a number to a power of **
 print("5**6= ", 5**6)
@@ -51,7 +51,6 @@ operation2 = (5+6)*8/2**2
 print(operation2)
 
 
-
-print (round(3.6))
-print ((3.6))
-print ((3.6))
+print(round(3.6))
+print((3.6))
+print((3.6))
--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-myVT : str = "Manchester United"
+myVT: str = "Manchester United"
 '''
 we havr 3 way to deal with on var nam:
 '''
-#snake_case ()
+# snake_case ()
 favorite_team = "AL Ittihad"
-#camelCase
-favoriteTeam =""
-#PascalCase ()
-#SomeCase
+# camelCase
+favoriteTeam = ""
+# PascalCase ()
+# SomeCase
 
-x:int = 9
-y:float = 9.2
+x: int = 9
+y: float = 9.2
 
 random = random1 = random2 = "multible assign"
 
