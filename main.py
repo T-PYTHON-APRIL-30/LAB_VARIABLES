8d762a2bb91cc87f3e7ff48091d3e829d4f3750f
Alaa <alaabinothman93@gmail.com>
2023-05-02 12:00:20 +0300

    Alaa Bin Othman, added prints

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+# علاء بن عثمان
+
+# question1
 favourite_football_team = "PSJ"
+print(favourite_football_team)
+# question2
 x: int = 10
 y: float = 5.5
+print(x, y)
+# question3
 myname = name = firstname = "Alaa"
+print(myname, name, firstname)
+# question4
 fname, mname, hight = "Alaa", "Ahmed", 176
+print(fname, mname, hight)
