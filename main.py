d76428dfc0d39c76f341e16734344f0d5ca0c023
SulimanBinM <sbinmohammed_1@icloud.com>
2023-05-02 12:01:59 +0300

    The lap is solved

---
+++ main.py
@@ -0,0 +1,19 @@
+favTeam = "Alittihad"
+print(favTeam)
+#------------------
+x = 7
+print(x)
+#------------------
+y = 9.6
+print(y)
+#------------------
+var1 = var2 = var3 = 10
+print(var1)
+#------------------
+var4 = var5 = var6 = "Hello World"
+print(var5)
+#------------------
+my_name , z , q = "Suliman" , 5 , 10.4
+print(my_name)
+print(z)
+print(q)
