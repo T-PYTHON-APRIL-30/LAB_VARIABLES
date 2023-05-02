8687fcae8e2a8eec66b14620371b6804532a44b5
Dalia <daliaasq@gmail.com>
2023-05-02 12:14:08 +0300

    First Lab

---
+++ main.py
@@ -0,0 +1,20 @@
+#Define a variable that has a value of your favourite football team.
+football_team = "Alhilal"
+print(football_team)
+
+#Define a variable x has an integer value and a variable y has a float value.
+x = 10
+y = 20.2
+print(x)
+print(y)
+
+#Assign One Value to Multiple Variables (choose random names & values).
+first_game = second_game = third_game = "Winner"
+print(first_game)
+print(second_game)
+print(third_game)
+
+#Assign multiple Values to Multiple Variables (choose random names & values).
+player_name, player_number = "Salem Aldossari", 10
+print(player_name)
+print(player_number)
