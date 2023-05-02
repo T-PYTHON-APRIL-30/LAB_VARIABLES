6145f292ff8cf6ec953d202e46f494508f04be64
Abdulaziz <Abdulaziz-A1997@hotmail.com>
2023-05-02 13:09:55 +0300

    Lab for variables has been solved

---
+++ var.py
@@ -0,0 +1,7 @@
+favourite_football_team = "AL-helal"
+x:int = 10
+y:float = 10.5
+var1 = var2 = var3 = 199
+var4, var5, var6 = "saad", 123, 12
+print(favourite_football_team, x, y, var1, var2, var3, var4, var5, var6)
+print(type(x),type(y))
