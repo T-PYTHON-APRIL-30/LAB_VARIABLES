34015c7e2ef9cba37a91a7b676ab954d7cbfb60d
Rayan Hantoul <ryan-055@outlook.com>
2023-05-02 14:29:31 +0300

    Solved Lap

--- Solved_Lap.py
+++ Solved_Lap.py
@@ -1,8 +1,12 @@
 My_fav_team = 'AL HILAL'
+print(My_fav_team)
 
 x = 10
 y = 5.5
+print(x,y)
 
-v1, v2, v3 = 'my_world'
+v1 = v2 = v3 = 'MyWorld '
+print(v1, v2, v3)
 
 F_name, L_name = 'Rayan ','Hantoul'
+print(F_name + L_name)
